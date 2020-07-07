74e7596270b236662f0cc16d43d5199fcf3b567b
Jenny Lam <jenny.lam@outreach.io>
2020-07-07 16:58:35 -0400

    Fetch a single lunch

--- work/client/src/pages/launch.tsx
+++ work/client/src/pages/launch.tsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { RouteComponentProps } from '@reach/router';
 
-interface LaunchProps extends RouteComponentProps {
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 
+import { Loading, Header, LaunchDetail } from "../components";
+import { ActionButton } from "../containers";
+import * as GetLaunchDetailsTypes from './__generated__/LaunchDetails';
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      id
+      site
+      isBooked
+      rocket {
+        id
+        name
+        type
+      }
+      mission {
+        name
+        missionPatch
+      }
+    }
+  }
+`;
+
+interface LaunchProps extends RouteComponentProps {
+  launchId? : any;
 }
 
-const Launch: React.FC<LaunchProps> = () => {
-  return <div />;
+const Launch: React.FC<LaunchProps> = ( {launchId} ) => {
+  const {
+    data,
+    loading,
+    error,
+  } = useQuery<
+    GetLaunchDetailsTypes.LaunchDetails,
+    GetLaunchDetailsTypes.LaunchDetailsVariables
+  >(GET_LAUNCH_DETAILS, { variables: {launchId} });
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+      <Fragment>
+        <Header image={data.launch && data.launch.mission && data.launch.mission.missionPatch}>
+          {data && data.launch && data.launch.mission && data.launch.mission.name}
+        </Header>
+        <LaunchDetail {...data.launch} />
+        <ActionButton {...data.launch} />
+      </Fragment>
+  );
 }
 
 export default Launch;
