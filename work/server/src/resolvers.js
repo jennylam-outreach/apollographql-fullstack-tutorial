b557736327a8dc0fcce03f5602517af537aee71f
Jenny Lam <jenny.lam@outreach.io>
2020-07-06 22:56:05 -0400

    Additional query resolvers

--- work/server/src/resolvers.js
+++ work/server/src/resolvers.js
@@ -5,5 +5,30 @@ module.exports = {
         launch: (_, { id }, { dataSources }) =>
             dataSources.launchAPI.getLaunchById({ launchId: id }),
         me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
-    }
+    },
+    Mission: {
+        missionPatch: (mission, {size} = {size: 'LARGE'}) => {
+            return size === 'SMALL'
+                ? mission.missionPatchSmall
+                : mission.missionPatchLarge
+        },
+    },
+    Launch: {
+        isBooked: async (launch, _, { dataSources }) =>
+            dataSources.userAPI.isBookedOnLaunch( {launchId: launch.id} ),
+    },
+    User : {
+        trips: async (_, __, { dataSources }) => {
+            const launchIds = await dataSources.getLaunchIdsByUser();
+            if (!launchIds.length) {
+                return [];
+            } else {
+                return (
+                    await dataSources.launchAPI.getLaunchesByIds(
+                        { launchIds }
+                    ) || []
+                );
+            }
+        },
+    },
 };
