8c30c77c4e0bcbc5b1bd42a8063deb114be02f22
Jenny Lam <jenny.lam@outreach.io>
2020-07-07 20:45:38 -0400

    Update data with useMutation

--- work/client/src/pages/login.tsx
+++ work/client/src/pages/login.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import ApolloClient from "apollo-client";
+
+import gql from "graphql-tag";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+
+import { LoginForm, Loading } from "../components";
+import * as LoginTypes from './__generated__/login';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const [login, { data }] = useMutation<LoginTypes.login, LoginTypes.loginVariables>(LOGIN_USER);
+  return <LoginForm login={login} />;
 }
